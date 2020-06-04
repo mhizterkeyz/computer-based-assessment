f6704e50caa6842f5eef96a41fffb004a8999b7c
Alikali Ojonugwa <alikali.ojonugwajustice@gmail.com>
2020-06-04 07:23:59 +0100

    Create credential component

--- src/components/credentials/CredentialsPage.tsx
+++ src/components/credentials/CredentialsPage.tsx
@@ -3,6 +3,49 @@ import { Redirect, useHistory } from 'react-router-dom';
 import './CredentialsPage.scss';
 import display_img from '../../image/Rectangle-19.png';
 
+const credentialsObj: CredentialsProps = {
+  displayImg: display_img,
+  name: "Alikali Ojonugwa Justice",
+  matric_no: "13MS1023",
+  department: "Computer Science",
+  faculty: "Natural Science"
+}
+
+interface CredentialsProps {
+  displayImg: string;
+  name: string;
+  matric_no: string;
+  department: string;
+  faculty: string;
+}
+
+const Credentials = (props: CredentialsProps) => {
+  return (
+    <>
+      <h3>Welcome {props.name} !</h3>
+      <div className="row d-flex justify-content-center align-items-center">
+        <div className="mr-5"><img src={props.displayImg} alt="ojay" /></div>
+        <div className="details">
+          <div>
+            <h4>Matric. no:</h4>
+            <h5>{props.matric_no}</h5>
+          </div>
+
+          <div>
+            <h4>Department:</h4>
+            <h5>{props.department}</h5>
+          </div>
+
+          <div>
+            <h4>Faculty:</h4>
+            <h5>{props.faculty}</h5>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
 const CredentialsPage = () => {
   const history = useHistory();
   const [redirectState, setRedirectState] = useState(false);
@@ -20,26 +63,7 @@ const CredentialsPage = () => {
     : (
       <section className="m-auto credentials">
         <div className="d-flex flex-column mb-5">
-          <h3>Welcome Alikali Ojonugwa Justice !</h3>
-          <div className="row d-flex justify-content-center align-items-center">
-            <div className="mr-5"><img src={display_img} alt="ojay" /></div>
-            <div className="details">
-              <div>
-                <h4>Matric. no:</h4>
-                <h5>13ms1023</h5>
-              </div>
-
-              <div>
-                <h4>Department:</h4>
-                <h5>Computer Science</h5>
-              </div>
-
-              <div>
-                <h4>Faculty:</h4>
-                <h5>Natural Science</h5>
-              </div>
-            </div>
-          </div>
+          <Credentials {...credentialsObj} />
 
           <p>If this page does not Auotmatically redirect you. <br /> Click on the proceed button.</p>
 
