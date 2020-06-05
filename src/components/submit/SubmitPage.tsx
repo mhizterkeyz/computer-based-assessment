37015b697fe1ec306c7c3423c15ec3d1cf5d48d6
Alikali Ojonugwa <alikali.ojonugwajustice@gmail.com>
2020-06-05 23:17:03 +0100

    Update submit page design

--- src/components/submit/SubmitPage.tsx
+++ src/components/submit/SubmitPage.tsx
@@ -15,13 +15,13 @@ const SubmitPage = () => {
   });
 
   return redirectState
-    ? <Redirect to="/exam/instruction" />
+    ? <Redirect to="/" />
     : (
       <section className="m-auto submit">
         <div>
-          <h3>Course - <span>NIGERIAN PEOPLE and culture (GST 103)</span></h3>
+          <h3>Course - <span>Nigeria People and Culture (GST 103)</span></h3>
         </div>
-        <div className="d-flex flex-column mb-5 submit">
+        <div className="d-flex flex-column mb-5 text-center submit">
           <p>Exam Submitted succesfully, Please Leave the Exam hall if you just Submitted <br />
         If this does not automatically redirect you to the Login Page</p>
 
