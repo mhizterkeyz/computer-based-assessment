f938665f690eb24f2f390c48d162e158e79c3737
Alikali Ojonugwa <alikali.ojonugwajustice@gmail.com>
2020-06-16 19:18:10 +0100

    Configure resux for data storage

---
+++ src/redux/configureStore.ts
@@ -0,0 +1,22 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import { rootReducer } from "./reducers";
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+export default function configureStore(initialState?: any) {
+  // const composeEnhancers = window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose || compose;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
+  );
+};
---
+++ src/redux/reducers/initialState.ts
@@ -0,0 +1,4 @@
+export default {
+  students: [],
+  apiCallsInProgress: 0
+};
