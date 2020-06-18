6127f0922398d03ce04806442d7be4d7c4f76153
Alikali Ojonugwa <alikali.ojonugwajustice@gmail.com>
2020-06-18 20:26:00 +0100

    delete student access token onSubmit

--- src/components/submit/SubmitPage.tsx
+++ src/components/submit/SubmitPage.tsx
@@ -1,44 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import { useHistory, Redirect } from 'react-router-dom';
-import './SubmitPage.scss';
+import React, { useState, useEffect } from "react";
+import { useHistory, Redirect } from "react-router-dom";
 
-const SubmitPage = () => {
-  const history = useHistory();
+import "./SubmitPage.scss";
+
+const SubmitPage = (props: { deleteStudent: () => Promise<any> }) => {
+  // const history = useHistory();
   const [redirectState, setRedirectState] = useState(false);
 
   useEffect(() => {
     const id = setTimeout(() => {
+      delete localStorage["jwt"];
+      delete localStorage["route"];
+      window.location.reload();
       setRedirectState(true);
-    }, 7000);
+    }, 5000);
 
     return () => clearTimeout(id);
   });
 
-  return redirectState
-    ? <Redirect to="/" />
-    : (
-      <section className="m-auto submit">
-        <div>
-          <h3>Course - <span>Nigeria People and Culture (GST 103)</span></h3>
-        </div>
-        <div className="d-flex flex-column mb-5 text-center submit">
-          <p>Exam Submitted succesfully, Please Leave the Exam hall if you just Submitted <br />
-        If this does not automatically redirect you to the Login Page</p>
+  return redirectState ? (
+    <Redirect to="/" />
+  ) : (
+    <section className="m-auto submit">
+      <div>
+        <h3>
+          Course - <span>Nigeria People and Culture (GST 103)</span>
+        </h3>
+      </div>
+      <div className="d-flex flex-column mb-5 text-center submit">
+        <p>
+          Exam Submitted succesfully, Please Leave the Exam hall if you just
+          Submitted <br />
+          If this does not automatically redirect you to the Login Page
+        </p>
 
-
-          <div className="text-center">
-            <button
-              className="btn"
-              onClick={() => {
-                history.push("/student-login");
-              }}
-            >
-              Click Here
-        </button>
-          </div>
+        <div className="text-center">
+          <button
+            className="btn"
+            onClick={() => {
+              delete localStorage["jwt"];
+              delete localStorage["route"];
+              window.location.reload();
+            }}
+          >
+            Click Here
+          </button>
         </div>
-      </section>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default SubmitPage;
